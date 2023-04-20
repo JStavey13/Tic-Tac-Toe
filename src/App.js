1ac79b861ffc84cb28632c27f7707dd507d62181
Jartius <JStavey13@users.noreply.github.com>
2023-04-20 16:52:30 -0400

    added first row to the board

--- src/App.js
+++ src/App.js
@@ -1,3 +1,15 @@
 export default function Square() {
-  return <button className="square">X</button>;
+  return (
+    <>
+    <div className="boardrow">
+      <button className="square">1</button>
+      <button className="square">2</button>
+      <button className="square">3</button>
+    </div>
+
+
+
+
+    </>
+  );
 }
