4e69de9e7e052ed58cf01cf195a6d13ba8bed75e
Jartius <JStavey13@users.noreply.github.com>
2023-04-20 16:56:37 -0400

    added all rows to the board

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 export default function Square() {
   return (
+    // a fragment tag for wrapping multiple elements
     <>
+    {/* the first board row */}
     <div className="boardrow">
       <button className="square">1</button>
       <button className="square">2</button>
       <button className="square">3</button>
     </div>
+
+    {/* the second board row */}
+    <div className="boardrow">
+      <button className="square">4</button>
+      <button className="square">5</button>
+      <button className="square">6</button>
+    </div>
+
+    {/* the third board row */}
+    <div className="boardrow">
+      <button className="square">1</button>
+      <button className="square">2</button>
+      <button className="square">3</button>
+    </div>
+
+
+
+
 
 
 
